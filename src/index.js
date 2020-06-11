c36a95c34dfe2e3e089ff179cff071b3a1caa559
zeena kareem <zena34269@gmail.com>
2020-06-11 15:03:31 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ for (let breed of keys){
           for (let b of data.message[breed]){
 
                  let ul=document.getElementById('dog-breeds')
-                 console.log(ul.lastChild)
+                 console.log(ul.lastChild..innerHTM='zzzzzzzzzzzzzz')
                         ul.lastChild.insertAdjacentHTML(
                                             "beforeend",
                                            ` <p>${b}</p>`
