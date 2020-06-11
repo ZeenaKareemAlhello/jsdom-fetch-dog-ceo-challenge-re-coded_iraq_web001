b3bc2bd2fa0704490a0cb549ddb0570d416cf033
zeena kareem <zena34269@gmail.com>
2020-06-11 14:59:47 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-console.log('%c HI', 'color: firebrick')
+//console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', () => {
+/*
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(response=>response.json())
+.then(data =>{
+  console.log('data',data)
+  data.message.forEach((image)=>{
+    let div=document.getElementById('dog-image-container')
+    div.insertAdjacentHTML(
+    "beforeend",
+    `  <img src=${image}>  `
+)  })
+})
+.catch(err=>console.log('error',err))
+/////*/
+fetch("https://dog.ceo/api/breeds/list/all")
+.then(response=>response.json())
+.then(data =>{
+  console.log('breed',data)
+let keys=Object.keys(data.message)
+console.log('keys',keys[5],'\n')
+//console.log(data.message[keys[5]])
+for (let breed of keys){
+  console.log(breed)
+
+  if(data.message[breed].length !==0){
+  console.log('breed',data.message.breed)
+          for (let b of data.message[breed]){
+
+                 let ul=document.getElementById('dog-breeds')
+                        ul.insertAdjacentHTML(
+                                            "beforeend",
+                                           ` <li><p>${b}</p> </li> `
+                                                )
+                      }
+}
+  }
+})
+.catch(err=>console.log('error',err))
+})
